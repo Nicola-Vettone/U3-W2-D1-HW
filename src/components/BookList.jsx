0754819d176ae1db9131876204d20f0320011d16
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-11 15:29:39 +0100

    secondo component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,58 +1,54 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Alert, Container, Form, Row, Col } from "react-bootstrap";
 import CommentArea from "./CommentArea";
 import SingleBook from "./SingleBook";
 
-class BookList extends Component {
-  state = {
-    searchQuery: "",
-    selectedBook: null
-  };
+const BookList = ({ books }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBook, setSelectedBook] = useState(null);
 
-  changeBookSelected = (book) => this.setState({ selectedBook: book });
+  const changeBookSelected = (book) => setSelectedBook(book);
 
-  render() {
-    return (
-      <Container className="BookList">
-        <Form.Control
-          className="mt-4"
-          type="text"
-          placeholder="Cerca un titolo"
-          value={this.state.searchQuery}
-          onChange={(e) => this.setState({ searchQuery: e.target.value })}
-        />
-        <Row className="mt-4">
-          <Col md={8}>
-            <Row xs={1} sm={2} md={3} lg={4}>
-              {this.props.books
-                .filter((book) => book.title.toLowerCase().includes(this.state.searchQuery.toLowerCase()))
-                .map((book) => (
-                  <SingleBook
-                    key={book.asin}
-                    book={book}
-                    changeBookSelected={this.changeBookSelected}
-                    selectedBookAsin={this.state.selectedBook ? this.state.selectedBook.asin : ""}
-                  />
-                ))}
-            </Row>
-          </Col>
-          <Col md={4}>
-            {this.state.selectedBook ? (
-              <CommentArea asin={this.state.selectedBook.asin} imgSrc={this.state.selectedBook.img} title={this.state.selectedBook.title} />
-            ) : (
-              <Alert variant="warning">👈Seleziona un libro per vederne le recensioni</Alert>
-            )}
-          </Col>
-        </Row>
+  return (
+    <Container className="BookList">
+      <Form.Control
+        className="mt-4"
+        type="text"
+        placeholder="Cerca un titolo"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <Row className="mt-4">
+        <Col md={8}>
+          <Row xs={1} sm={2} md={3} lg={4}>
+            {books
+              .filter((book) => book.title.toLowerCase().includes(searchQuery.toLowerCase()))
+              .map((book) => (
+                <SingleBook
+                  key={book.asin}
+                  book={book}
+                  changeBookSelected={changeBookSelected}
+                  selectedBookAsin={selectedBook ? selectedBook.asin : ""}
+                />
+              ))}
+          </Row>
+        </Col>
+        <Col md={4}>
+          {selectedBook ? (
+            <CommentArea asin={selectedBook.asin} imgSrc={selectedBook.imgSrc} title={selectedBook.title} />
+          ) : (
+            <Alert variant="warning">👈Seleziona un libro per vederne le recensioni</Alert>
+          )}
+        </Col>
+      </Row>
 
-        {this.props.books.length === 0 && (
-          <Alert variant="warning" className="mt-4">
-            Premi un bottone per visualizzare dei libri👆
-          </Alert>
-        )}
-      </Container>
-    );
-  }
-}
+      {books.length === 0 && (
+        <Alert variant="warning" className="mt-4">
+          Premi un bottone per visualizzare dei libri👆
+        </Alert>
+      )}
+    </Container>
+  );
+};
 
 export default BookList;
