3306b4e2f27e542c4e232a1d742ab0d7a70ef16a
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-11 16:37:59 +0100

    fatto

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 import BookList from "./components/BookList";
 import Footer from "./components/Footer";
@@ -12,39 +12,38 @@ import romance from "./data/books/romance.json";
 import scifi from "./data/books/scifi.json";
 import { Button } from "react-bootstrap";
 
-class App extends Component {
-  state = {
+const App = () => {
+  /*  state = {
     books: fantasy
-  };
+  }; */
+  const [books, setbooks] = useState(fantasy);
 
-  render() {
-    console.log("render App");
-    return (
-      <>
-        <MyNav />
-        <Welcome />
-        <div className="d-flex justify-content-center gap-1">
-          <Button variant="primary" onClick={() => this.setState({ books: fantasy })}>
-            Fantasy
-          </Button>
-          <Button variant="warning" onClick={() => this.setState({ books: history })}>
-            History
-          </Button>
-          <Button variant="danger" onClick={() => this.setState({ books: horror })}>
-            Horror
-          </Button>
-          <Button variant="success" onClick={() => this.setState({ books: romance })}>
-            Romance
-          </Button>
-          <Button variant="info" onClick={() => this.setState({ books: scifi })}>
-            Scifi
-          </Button>
-        </div>
-        <BookList books={this.state.books} />
-        <Footer />
-      </>
-    );
-  }
-}
+  console.log("render App");
+  return (
+    <>
+      <MyNav />
+      <Welcome />
+      <div className="d-flex justify-content-center gap-1">
+        <Button variant="primary" onClick={() => setbooks(fantasy)}>
+          Fantasy
+        </Button>
+        <Button variant="warning" onClick={() => setbooks(history)}>
+          History
+        </Button>
+        <Button variant="danger" onClick={() => setbooks(horror)}>
+          Horror
+        </Button>
+        <Button variant="success" onClick={() => setbooks(romance)}>
+          Romance
+        </Button>
+        <Button variant="info" onClick={() => setbooks(scifi)}>
+          Scifi
+        </Button>
+      </div>
+      <BookList books={books} />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
